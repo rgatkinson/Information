4dfdfa7dc0213335f767bd550a4e360a495bbf49
RobertAtkinson <bob@theatkinsons.org>
2015-10-13 23:33:23 -0700

    more cleanup

--- FtcRobotController/app/src/main/java/com/ftdi/j2xx/e.java
+++ FtcRobotController/app/src/main/java/com/ftdi/j2xx/e.java
@@ -1,3 +1,8 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package com.ftdi.j2xx;
 
 import com.ftdi.j2xx.FT_Device;
@@ -5,106 +10,152 @@ import com.ftdi.j2xx.FT_EEPROM;
 import com.ftdi.j2xx.k;
 
 class e extends k {
-   e(FT_Device var1) {
-      super(var1);
-   }
+    e(FT_Device var1) {
+        super(var1);
+    }
 
-   int a(byte[] var1) {
-      if(var1.length <= this.b()) {
-         int[] var2 = new int[64];
+    short a(FT_EEPROM var1) {
+        int[] var2 = new int[64];
+        if(var1.getClass() != FT_EEPROM.class) {
+            return (short)1;
+        } else {
+            FT_EEPROM var3 = var1;
 
-         for(short var3 = 0; var3 < 64; ++var3) {
-            var2[var3] = this.a(var3);
-         }
+            try {
+                var2[1] = var3.VendorId;
+                var2[2] = var3.ProductId;
+                var2[3] = 512;
+                var2[4] = this.a((Object)var1);
+                byte var4 = 10;
+                int var7 = this.a(var3.Manufacturer, var2, var4, 7, true);
+                var7 += var3.Manufacturer.length() + 2;
+                var7 = this.a(var3.Product, var2, var7, 8, true);
+                var7 += var3.Product.length() + 2;
+                if(var3.SerNumEnable) {
+                    var7 = this.a(var3.SerialNumber, var2, var7, 9, true);
+                    int var10000 = var7 + var3.SerialNumber.length() + 2;
+                }
 
-         short var4 = (short)('\uffff' & (short)(-1 + (63 - this.b() / 2)));
+                if(var2[1] != 0 && var2[2] != 0) {
+                    boolean var5 = false;
+                    var5 = this.a(var2, 63);
+                    return (short)(var5?0:1);
+                } else {
+                    return (short)2;
+                }
+            } catch (Exception var6) {
+                var6.printStackTrace();
+                return (short)0;
+            }
+        }
+    }
 
-         short var8;
-         for(int var5 = 0; var5 < var1.length; var4 = var8) {
-            int var6;
-            if(var5 + 1 < var1.length) {
-               var6 = 255 & var1[var5 + 1];
+    FT_EEPROM a() {
+        FT_EEPROM var1 = new FT_EEPROM();
+        int[] var2 = new int[64];
+
+        try {
+            int var3;
+            for(var3 = 0; var3 < 64; ++var3) {
+                var2[var3] = this.a((short)var3);
+            }
+
+            var1.VendorId = (short)var2[1];
+            var1.ProductId = (short)var2[2];
+            this.a(var1, var2[4]);
+            byte var5 = 10;
+            var1.Manufacturer = this.a(var5, var2);
+            var3 = var5 + var1.Manufacturer.length() + 1;
+            var1.Product = this.a(var3, var2);
+            var3 += var1.Product.length() + 1;
+            var1.SerialNumber = this.a(var3, var2);
+            return var1;
+        } catch (Exception var4) {
+            return null;
+        }
+    }
+
+    int b() {
+        int var1 = this.a((short)7);
+        int var2 = (var1 & '\uff00') >> 8;
+        var2 /= 2;
+        var1 = this.a((short)8);
+        int var3 = (var1 & '\uff00') >> 8;
+        var3 /= 2;
+        int var4 = 10 + var2 + var3 + 1;
+        var1 = this.a((short)9);
+        int var5 = (var1 & '\uff00') >> 8;
+        var5 /= 2;
+        return (63 - var4 - 1 - var5) * 2;
+    }
+
+    int a(byte[] var1) {
+        boolean var2 = false;
+        boolean var3 = false;
+        if(var1.length > this.b()) {
+            return 0;
+        } else {
+            int[] var4 = new int[64];
+
+            for(short var5 = 0; var5 < 64; ++var5) {
+                var4[var5] = this.a(var5);
+            }
+
+            short var7 = (short)(63 - this.b() / 2 - 1);
+            var7 = (short)(var7 & '\uffff');
+
+            for(int var8 = 0; var8 < var1.length; var8 += 2) {
+                int var6;
+                if(var8 + 1 < var1.length) {
+                    var6 = var1[var8 + 1] & 255;
+                } else {
+                    var6 = 0;
+                }
+
+                var6 <<= 8;
+                var6 |= var1[var8] & 255;
+                var4[var7++] = var6;
+            }
+
+            if(var4[1] != 0 && var4[2] != 0) {
+                boolean var9 = false;
+                var9 = this.a(var4, 63);
+                if(!var9) {
+                    return 0;
+                } else {
+                    return var1.length;
+                }
             } else {
-               var6 = 0;
+                return 0;
+            }
+        }
+    }
+
+    byte[] a(int var1) {
+        boolean var2 = false;
+        boolean var3 = false;
+        boolean var4 = false;
+        byte[] var5 = new byte[var1];
+        if(var1 != 0 && var1 <= this.b() - 1) {
+            short var6 = (short)(63 - this.b() / 2 - 1);
+            var6 = (short)(var6 & '\uffff');
+
+            for(int var7 = 0; var7 < var1; var7 += 2) {
+                int var10 = this.a(var6++);
+                if(var7 + 1 < var5.length) {
+                    byte var8 = (byte)(var10 & 255);
+                    var5[var7 + 1] = var8;
+                } else {
+                    var3 = false;
+                }
+
+                byte var9 = (byte)((var10 & '\uff00') >> 8);
+                var5[var7] = var9;
             }
 
-            int var7 = var6 << 8 | 255 & var1[var5];
-            var8 = (short)(var4 + 1);
-            var2[var4] = var7;
-            var5 += 2;
-         }
-
-         if(var2[1] != 0 && var2[2] != 0 && this.a(var2, 63)) {
-            return var1.length;
-         }
-      }
-
-      return 0;
-   }
-
-   FT_EEPROM a() {
-      // $FF: Couldn't be decompiled
-   }
-
-   short a(FT_EEPROM var1) {
-      int[] var2 = new int[64];
-      if(var1.getClass() != FT_EEPROM.class) {
-         return (short)1;
-      } else {
-         boolean var6;
-         try {
-            var2[1] = var1.VendorId;
-            var2[2] = var1.ProductId;
-            var2[3] = 512;
-            var2[4] = this.a((Object)var1);
-            int var4 = this.a(var1.Manufacturer, var2, 10, 7, true) + 2 + var1.Manufacturer.length();
-            int var5 = this.a(var1.Product, var2, var4, 8, true) + 2 + var1.Product.length();
-            if(var1.SerNumEnable) {
-               int var10000 = this.a(var1.SerialNumber, var2, var5, 9, true) + 2 + var1.SerialNumber.length();
-            }
-
-            if(var2[1] == 0 || var2[2] == 0) {
-               return (short)2;
-            }
-
-            var6 = this.a(var2, 63);
-         } catch (Exception var7) {
-            var7.printStackTrace();
-            return (short)0;
-         }
-
-         return (short)(var6?0:1);
-      }
-   }
-
-   byte[] a(int var1) {
-      byte[] var2 = new byte[var1];
-      if(var1 != 0 && var1 <= -1 + this.b()) {
-         short var3 = (short)('\uffff' & (short)(-1 + (63 - this.b() / 2)));
-
-         short var5;
-         for(int var4 = 0; var4 < var1; var3 = var5) {
-            var5 = (short)(var3 + 1);
-            int var6 = this.a(var3);
-            if(var4 + 1 < var2.length) {
-               byte var7 = (byte)(var6 & 255);
-               var2[var4 + 1] = var7;
-            }
-
-            var2[var4] = (byte)((var6 & '\uff00') >> 8);
-            var4 += 2;
-         }
-      } else {
-         var2 = null;
-      }
-
-      return var2;
-   }
-
-   int b() {
-      int var1 = (('\uff00' & this.a((short)7)) >> 8) / 2;
-      int var2 = 1 + (('\uff00' & this.a((short)8)) >> 8) / 2 + var1 + 10;
-      int var3 = (('\uff00' & this.a((short)9)) >> 8) / 2;
-      return 2 * (-1 + (63 - var2) - var3);
-   }
+            return var5;
+        } else {
+            return null;
+        }
+    }
 }
